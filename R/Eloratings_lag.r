9936418dced5c643b83a589b9bc8597df5799778
jzwolski <jzwolski@iastate.edu>
2015-11-03 00:10:28 -0600

    Elo ratings functions

--- R/Eloratings_lag.r
+++ R/Eloratings_lag.r
@@ -1,11 +1,11 @@
 #' Creates elo ratings for players within a specified time frame with a one month lag before the specified time frame.
 #' 
-#' @param d1 The beginning date for ratings.
-#' @param d2 The end date for ratings.
+#' @param date1 The beginning date for ratings.
+#' @param date2 The end date for ratings.
 #' @param x A data frame where first column is the number of days from a specified date, the second column is Player 1, the third column is Player 2, and the last column is the result, either a 0 or 1.
 #' @return A list of elo ratings.
 #' @export
-Eloratings_lag <- function(d1, d2, x){
+Eloratings_lag <- function(date1, date2, x){
   lagdate1 <- date1 %m-% months(1)
   lagdate2 <- date1 - 1
   lagdays1 <- lagdate1 - beginningdate
@@ -20,5 +20,6 @@ Eloratings_lag <- function(d1, d2, x){
   days2 <- date2 - beginningdate
   results.subset <- x[which((x$Day>=days1)&(x$Day<=days2)),]
   elo.subset <- elo(results.subset, status=elo.lagsubset, init=1, sort=T)
+  fullresults <- rbind(results.lagsubset, results.subset)
   return(elo.subset)
 }
