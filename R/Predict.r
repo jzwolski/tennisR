2bfd691fb1eedefbffb649ac8d98b6c3a77ab393
jzwolski <jzwolski@iastate.edu>
2015-11-17 02:18:35 -0600

    Prediction function

--- R/Predict.r
+++ R/Predict.r
@@ -3,14 +3,16 @@
 #' @param player1 First player in the head to head matchup.
 #' @param player2 Second player in the head to head matchup.
 #' @param elo A set of elo ratings, in the original elo ratings format.
+#' @param games Total number of games played by either player.
+#' @param results A subset of match results (the second list returned from either elo ratings functions)
 #' @return A probability that player 1 will win the tennis match.
 #' @export
-Predict <- function(player1, player2, elo, results){
+Predict <- function(player1, player2, elo, games, results){
   matchup <- data.frame(player1, player2)
   matchup$Day <- NA
   matchup <- matchup[c(3,1,2)]
   
-  predict <- predict(elo, matchup, tng=5, gamma=0)
+  predict <- predict(elo, matchup, tng=games, gamma=0)
   predict <- data.frame(predict)
   predict.compare <- cbind(matchup, predict)
   predict.compare <- predict.compare[,-1]
@@ -37,5 +39,9 @@ Predict <- function(player1, player2, elo, results){
       }
     }
   }
-  return(predict.compare$predict)
+  probability <- data.frame(predict.compare$player1,predict.compare$player2,predict.compare$predict)
+  names(probability)[1] <- "Player1"
+  names(probability)[2] <- "Player2"
+  names(probability)[3] <- "Probability"
+  return(probability)
 }
